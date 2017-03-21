7e2ba4369c825440b76f61db0911370bc962b6b9
Rajan Kasodariya <kasodariyarajan@gmail.com>
2017-03-21 15:45:51 +0530

    files added

---
+++ LiveVmMigration/src/Migration/Config.java
@@ -0,0 +1,13 @@
+package Migration;
+
+public class Config {
+	
+	/* details of Source */
+	public static String sourceIP="172.16.2.53";
+	public static int sourcePORT=1053;
+	
+	/* details of Source */
+	public static String destinationIP="172.16.2.53";
+	public static int destinationPORT=1053;	
+	
+}
--- LiveVmMigration/src/Migration/VM.java
+++ LiveVmMigration/src/Migration/VM.java
@@ -1,5 +1,12 @@
 package Migration;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javax.print.attribute.standard.PrinterLocation;
+
 public class VM {
 	/* Variables */
 	private int stackSize;		// stack size
@@ -17,15 +24,49 @@ public class VM {
 	
 	boolean dirty[];			// shows id page is dirtied or not ?
 	
-	public VM(int code[]){
-		stackSize=1000;
+	public VM(int code[],int stackSize){
+		this.stackSize=stackSize;
 		ip=0;
 		sp=0;
 		
 		this.code=code;
-		global=new int[1000];
-		stack=new int[1000];
+		global=new int[stackSize];
+		stack=new int[stackSize];
 	}
 	
+	/*
+	 * Displays content of stack
+	 * */
+	public void stackDisplay(){
+		for(int i=0;i<stackSize;i++){
+			if(i%11==0) System.out.println("");
+			System.out.println(stack[i]+" ");
+		}
+	}
 	
+	/*
+	 * Sends complete VM to destination
+	 * 
+	 * Sender behaves as a Server
+	 * */
+	
+	public void sendVM(){
+		try {
+			ServerSocket sc=new ServerSocket();
+			
+			// Accept client request
+			Socket client=new Socket();
+			
+			ObjectOutputStream op=new ObjectOutputStream(client.getOutputStream());
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/*
+	 * Receives complete VM at destination
+	 * */
 }
